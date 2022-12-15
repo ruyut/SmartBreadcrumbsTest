69ebd4fbbd57750c391852e8821eebd95d550010
Ruyut <a@ruyut.com>
2022-12-15 12:06:53 +0800

    init commit

---
+++ WebApplicationMvcSmartBreadcrumbsTest/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApplicationMvcSmartBreadcrumbsTest.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
